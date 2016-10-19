2d8a86dbd9712baa4e14ec3ec1472fd58b85245f
magmabee <luimartinez593@gmail.com>
2016-10-19 17:29:37 -0500

    fixed icon positions and cleaned up some code

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ var player = prompt("Rock, paper, or scissors?");
 var player = player.toLowerCase();
 var roll = Math.random();
 var result;
-var player2LabelID = document.getElementById("player2label")
+var player2Hands = document.getElementById("player2");
 
 //identification of computer choice and changes to the dom to reflect that
 if (roll <= 0.33)
 {
   roll = "rock";
-  player2LabelID.innerHTML = "Rock";
-  document.getElementById("player2").innerHTML = "<i class=\"fa fa-hand-rock-o\" style=\"font-size:196px;\"></i>";
+  document.getElementById("player2label").innerHTML = "<span class='p.label'>Rock</span>";
+  player2Hands.className = "hands";
+  player2Hands.innerHTML = "<i class=\"fa fa-hand-rock-o\"></i>";
 }
 else if (roll <= 0.66 && roll > 0.33) {
   roll = "paper";
-  player2LabelID.innerHTML = "Paper";
-  document.getElementById("player2").innerHTML = "<i class=\"fa fa-hand-paper-o\" style=\"font-size:196px;\"></i>";
+  document.getElementById("player2label").innerHTML = "<span class='p.label'>Paper</span>";
+  player2Hands.className = "hands";
+  player2Hands.innerHTML = "<i class=\"fa fa-hand-paper-o\"></i>";
 }
 else {
   roll = "scissors";
-  player2LabelID.innerHTML = "Scissors";
-  document.getElementById("player2").innerHTML = "<i class=\"fa fa-hand-scissors-o\" style=\"font-size:196px;\"></i>";
+  document.getElementById("player2label").innerHTML = "<span class='p.label'>Scissors</span>";
+  player2Hands.className = "hands";
+  player2Hands.innerHTML = "<i class=\"fa fa-hand-scissors-o\"></i>";
   }
 
 /*changes for player text to identify whether he used r, p, or s
@@ -30,7 +33,7 @@ if (player = "rock")
 else if(player = "paper"){
   document.getElementById("player1label").innerHTML = "Paper";
 }
-else{
+else if (player = "scissors"){
   document.getElementById("player1label").innerHTML = "Scissors";
 }
 */
@@ -38,6 +41,7 @@ else{
 var winners = function(results1, results2){
   if(results1 === results2){
       window.result = "Draw";
+      document.getElementById("message").style.color = "#4d0026";
       document.getElementById("message").innerHTML = "Draw!";
   }
   else if (results1 === "rock") {
@@ -76,6 +80,9 @@ var winners = function(results1, results2){
     }
   }
 
+function replay(){
+    location.reload();
+}
 
 winners(player, roll);
 
